898691678bca5edc3ff49171cec4ba6cb4c057b9
Zharell <javi_lopez-calderon@hotmail.com>
2022-09-27 21:15:42 +0200

    27/09

---
+++ DAM2/Celular/Ejercicios Temporada 1/ProjectGitHub/app/src/main/java/com/example/projectgithub/utt1.java
@@ -0,0 +1,14 @@
+package com.example.projectgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class utt1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utt1);
+    }
+}
